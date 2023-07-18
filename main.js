d7f1214da202af92f3c4be5011540a1aa8023d1c
kevin <94207816+KevinV0212@users.noreply.github.com>
2023-07-18 19:33:04 -0400

    toggle form

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 let myLibrary = [];
+
+// Book object constructor
 function Book(title, author, numPages, isRead) {
     this.title = title;
     this.author = author;
@@ -21,6 +23,7 @@ function addBookToLibrary(title, author, numPages, isRead) {
 
 const bookshelf = document.querySelector('.bookshelf');
 
+// loops through myLibrary and displays all books as 'card' elements
 function displayBookshelf()
 {
     myLibrary.forEach(function(book){
@@ -34,3 +37,12 @@ function displayBookshelf()
 addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 255, true);
 console.log(myLibrary);
 displayBookshelf();
+
+const formToggler = document.querySelector('.form-toggler')
+const formWrapper = document.querySelector('.form-wrapper')
+formToggler.addEventListener('click', (e) => {
+    if (formWrapper.style.display === 'none')
+        formWrapper.style.display = 'block'
+    else 
+        formWrapper.style.display = 'none'
+})
